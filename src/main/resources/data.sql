1250641b674fcfa9954ba11eb7d7c35bd2c5caf4
SUYEON KIM <waterkite94@gmail.com>
2023-08-01 00:51:27 +0900

    Feat: Add Test Config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+drop table if exists member;
+drop table if exists post;
+drop table if exists post_detail;
+
+create table member
+(
+    id         bigint unsigned primary key auto_increment,
+    email      varchar(255) not null,
+    password   varchar(40)  not null,
+    name       varchar(40)  not null,
+    username   varchar(40)  not null,
+    created_at timestamp    not null,
+    updated_at timestamp    not null
+);
+
+create table post
+(
+    id         bigint unsigned primary key auto_increment,
+    title      varchar(255)     not null,
+    view_count integer unsigned not null,
+    user_id    bigint unsigned  not null,
+    created_at timestamp        not null,
+    updated_at timestamp        not null
+);
+
+create table post_detail
+(
+    post_id    bigint unsigned primary key,
+    content    varchar(10000) not null,
+    created_at timestamp      not null,
+    updated_at timestamp      not null
+);
