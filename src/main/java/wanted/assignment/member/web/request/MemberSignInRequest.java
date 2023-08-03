90dbd861d4c09fc46de4e090c29fb9d5ae94ab6d
SUYEON KIM <waterkite94@gmail.com>
2023-08-03 16:47:25 +0900

    Feat: Add SignIn, SignUp Validation

--- src/main/java/wanted/assignment/member/web/request/MemberSignInRequest.java
+++ src/main/java/wanted/assignment/member/web/request/MemberSignInRequest.java
@@ -2,6 +2,7 @@ package wanted.assignment.member.web.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -15,6 +16,7 @@ public class MemberSignInRequest {
 	@Email(message = "올바른 이메일 형식으로 입력해주세요.")
 	private final String email;
 
+	@NotNull(message = "비밀번호는 필수값입니다.")
 	@Length(min = 8, message = "비밀번호는 8자 이상 입력해주세요.")
 	private final String password;
 
