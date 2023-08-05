cc17e00de69515d2a48ae270cfcbd746acbfd3e0
SUYEON KIM <waterkite94@gmail.com>
2023-08-05 09:14:46 +0900

    Feat: Add docker files

---
+++ mysql/initdb.d/create.sql
@@ -0,0 +1,26 @@
+create table member
+(
+    id         bigint unsigned primary key auto_increment,
+    email      varchar(255) unique not null,
+    password   char(64)            not null,
+    created_at timestamp           not null,
+    updated_at timestamp           not null
+);
+
+create table post
+(
+    id         bigint unsigned primary key auto_increment,
+    title      varchar(255) not null,
+    view_count integer unsigned not null,
+    user_id    bigint unsigned not null,
+    created_at timestamp    not null,
+    updated_at timestamp    not null
+);
+
+create table post_detail
+(
+    post_id    bigint unsigned primary key,
+    content    varchar(10000) not null,
+    created_at timestamp      not null,
+    updated_at timestamp      not null
+);
